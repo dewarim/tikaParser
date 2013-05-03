03787fbb3511c4d89bda5f8d40253ae63ac9d5f9
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-05-03 14:12:36 +0200

    upgraded to Grails 2.2.2. Fixes #4

--- TikaParserGrailsPlugin.groovy
+++ TikaParserGrailsPlugin.groovy
@@ -1,16 +1,15 @@
 class TikaParserGrailsPlugin {
 
-    def version = "1.3.0"
+    def version = "1.3.0.1"
     def grailsVersion = "2.0 > *"
     def title = "Apache Tika Plugin"
     def author = "Ingo Wiarda"
     def authorEmail = "ingo.wiarda@texolution.eu"
     def description = 'Bundles the Apache Tika along with a parser service.'
-
     def documentation = "http://grails.org/plugin/tikaParser"
-
     def license = 'APACHE'
-    def organization = [ name: "Horner GmbH", url: "http://www.horner-project.de/" ]
+    def organization = [ name: "Texolution GmbH", url: "http://texolution.eu/" ]
     def issueManagement = [ system: "github", url: "https://github.com/dewarim/tikaParser/issues" ]
-    def scm = [ url: "https://github.com/dewarim/tikaParser" ]
+    def scm = [ url: "https://github.com/dewarim/tikaParser"
+    ]
 }
